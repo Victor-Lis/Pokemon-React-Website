cee4be0859ffee90b43b33854d8a4ec7d3049934
Victor-Lis <victorlisbronzo1@gmail.com>
2023-01-14 13:57:21 -0300

    v1

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -33,7 +33,7 @@ const Pokemon = ({pokemon}) => {
 
         <div className='pokemon-header-title-area'>
 
-          <h3 className='pokemon-header-title'> Name: {pokemonDatas?.name}  </h3>
+          <div className='pokemon-header-title'> <h3 className='pokemon-header-title-name'> Name: </h3> <h3 className='pokemon-header-title-pokemon'> {pokemonDatas?.name} </h3> </div>
           <h3 className='pokemon-header-title'> Id: {pokemonDatas?.id} </h3>
 
         </div>
@@ -47,6 +47,33 @@ const Pokemon = ({pokemon}) => {
             {pokemonDatas.types != undefined && pokemonDatas.types.map(index => <p className='pokemon-header-type-p' key={Math.random()}> {index.type.name} </p>)}
           
           </div>  
+
+        </div>
+
+      </div>
+
+      <div className='pokemon-main'>
+
+        <div className='pokemon-main-img-area'>
+
+          {pokemonDatas.sprites != undefined && <img className='pokemon-main-img' src={pokemonDatas.sprites.front_default} />}
+          {pokemonDatas.sprites != undefined && <img className='pokemon-main-img' src={pokemonDatas.sprites.back_default} />}
+
+        </div>
+
+        <div className='pokemon-main-stats-area'>
+
+          <div className='pokemon-main-stats-column'>
+
+              {pokemonDatas.stats != undefined && pokemonDatas.stats.map(index => <p className='first-letter'> {index.stat.name}: </p>)}
+
+          </div>
+
+          <div className='pokemon-main-stats-column'>
+
+              {pokemonDatas.stats != undefined && pokemonDatas.stats.map(index => <p> {index.base_stat} </p>)}
+
+          </div>
 
         </div>
 
